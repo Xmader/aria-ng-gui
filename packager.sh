6b6bde4fddea71d972cc58e4c13b4e257b97700e
Xmader <xmader@outlook.com>
2018-09-24 19:52:50 +0800

    init

--- packager.sh
+++ packager.sh
@@ -6,16 +6,8 @@ else
    arch=x64
 fi
 
-if [ ${platform} = "darwin" -a $(uname -o) = "Msys" ]
-then
-    echo "由于系统限制，目前只能在*unix系统上打包darwin(MacOS)版的APP"
-    exit
-fi
-
-cd 
-
 electron-packager . AriaNgGUI \
-    --platform=${platform} --arch=${arch} \
+    --platform=${platform} --arch=${arch} --electron-version 2.0.10 \
     --out OutApp/ --overwrite \
     --icon=./assets/AriaNg.ico --ignore=packager.sh \
     --tmpdir=OutApp/Temp/ --download.cache=OutApp/Temp/ --download.mirror="https://npm.taobao.org/mirrors/electron/"
