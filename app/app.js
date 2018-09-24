4255453b19fff4ed5fb039a9c0ff943346c859ef
Xmader <xmader@outlook.com>
2018-09-24 20:58:05 +0800

    v1.2.0

--- app/app.js
+++ app/app.js
@@ -28,7 +28,7 @@ app.on('ready', function () {
         }
     });
 
-    var aria2_dir = __dirname + '/aria2/' + aria2_bin
+    var aria2_dir = `${__dirname}/aria2/${platform}/${aria2_bin}`
 
     //打开主程序
     var subpy = require('child_process').spawn(aria2_dir, [`--conf-path=${__dirname}/aria2/aria2.conf`]);
