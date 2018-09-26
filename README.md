2b71d5dab3912e7260077bd2bcf13e2ffffb06b8
Xmader <xmader@outlook.com>
2018-09-26 11:27:14 +0800

    v1.4.0 - 支持打开下载路径和在文件管理器中显示文件

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 
 > 一个 Aria2 图形界面客户端
 
-[![构建状态](https://dev.azure.com/xmader/aria-ng-gui/_apis/build/status/aria-ng-gui-darwin)](https://dev.azure.com/xmader/aria-ng-gui/_build/latest?definitionId=1)
-
 ![](https://aria-ng.xmader.com/Screenshot1.png)
 
 ## 特性
@@ -29,8 +27,6 @@
 # AriaNg GUI
 
 > An Aria2 GUI for Windows & Linux & MacOS
-
-[![Build Status](https://dev.azure.com/xmader/aria-ng-gui/_apis/build/status/aria-ng-gui-darwin)](https://dev.azure.com/xmader/aria-ng-gui/_build/latest?definitionId=1)
 
 ![](https://aria-ng.xmader.com/en/Screenshot1.png)
 
--- app/pre.js
+++ app/pre.js
@@ -10,9 +10,10 @@
  * 
 */
 
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, shell, remote: { app } } = require('electron');
 const check_update = require('./check_update.js')
 
+// 添加右键菜单
 window.addEventListener('contextmenu', (e) => {
     e.preventDefault();
 
@@ -24,4 +25,28 @@ window.addEventListener('contextmenu', (e) => {
     }
 });
 
-check_update() // 检查版本更新
+// 检查版本更新
+check_update()
+
+// 打开下载路径
+const open_download_dir = (dir) => {
+    shell.openItem(dir)
+    shell.beep()
+}
+
+window.open_download_dir = open_download_dir
+
+// 在文件管理器中显示文件
+const show_file = (path) => {
+    shell.showItemInFolder(path)
+    shell.beep()
+}
+
+window.show_file = show_file
+
+// 显示AriaNg GUI的版本号
+window.onload = () => {
+    const version = app.getVersion()
+    let logo = document.getElementById("aria-ng-logo")
+    logo.title = `AriaNg GUI v${version} | ${logo.title}`
+}
