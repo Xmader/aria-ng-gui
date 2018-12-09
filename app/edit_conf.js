e2508af340d81957821c56b127c690bf03770b34
Xmader <xmader@outlook.com>
2018-12-09 16:47:34 +0800

    统一代码风格

--- app/edit_conf.js
+++ app/edit_conf.js
@@ -10,10 +10,10 @@
  * 
 */
 
-const os = require('os');
-const fs = require('fs')
-const path = require('path')
-const { app } = require('electron')
+const os = require("os")
+const fs = require("fs")
+const path = require("path")
+const { app } = require("electron")
 
 const default_download_dir = app.getPath("downloads") || path.join(os.homedir(), "Downloads")
 
@@ -22,17 +22,17 @@ const edit_conf = (conf_path) => {
 
     let old_conf = fs.readFileSync(conf_path).toString()
 
-	let download_dir = default_download_dir;
-	let saved_dir = (old_conf.match(/^dir=(.*)$/m) || ['', '']) [1];
-	if (saved_dir) {
-		var stat = null;
-		try {
-			stat = fs.statSync(saved_dir)
-		} catch (e) {}
-		if (stat && stat.isDirectory()) {
-			download_dir = saved_dir
-		}
-	}
+    let download_dir = default_download_dir
+    let saved_dir = (old_conf.match(/^dir=(.*)$/m) || ["", ""])[1]
+    if (saved_dir) {
+        let stat = null
+        try {
+            stat = fs.statSync(saved_dir)
+        } catch (e) { }
+        if (stat && stat.isDirectory()) {
+            download_dir = saved_dir
+        }
+    }
 
     let new_conf = old_conf
         .replace(/^dir=.*$/m, "dir=" + download_dir)
